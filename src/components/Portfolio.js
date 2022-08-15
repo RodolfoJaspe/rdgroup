1eb7fe94c1688970ae497196eecf888516f65538
Rodolfo Jaspe <68761286+RodolfoJaspe@users.noreply.github.com>
2022-08-14 22:40:27 -0400

    responsive portfolio sliders

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -75,7 +75,7 @@ export default function Portfolio() {
                         <img src='https://drive.google.com/uc?export=view&id=1Q-qaTTbYVJgxY1pE4-a6dKMu1b0Fj-tn' alt="logo"/>
                         <h2>{category.category}</h2>
                     </div>
-                  <Slider images={category.images} />  
+                    <Slider images={category.images} />  
                 </div>
             </div>
         ))}
