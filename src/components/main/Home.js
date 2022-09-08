69d37a80fcb127d6d0e413bf27aa8d4a80f189b7
Rodolfo Jaspe <68761286+RodolfoJaspe@users.noreply.github.com>
2022-09-08 14:27:18 -0400

    fixed h1 sizes

--- src/components/main/Home.js
+++ src/components/main/Home.js
@@ -9,7 +9,12 @@ function Home() {
   return (
         <div className='home'>
             <div className='logo-slogan'>
-                <img src={logo.url}       className='logo' alt="logo image" width={500} height={500} title={logo.title} loading='lazy'/>
+                <img 
+                    src={logo.url}       
+                    className='logo' 
+                    alt="logo image" 
+                    width={500} height={500} 
+                    title={logo.title}/>
                 <div className='slogan'>
                     <h1><b>r i g h t </b><b> d e s i g n</b></h1>
                 </div>  
